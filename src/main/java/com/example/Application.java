e2a33a860dfa1e09d5b2d9b89c74d82b242fc486
Adesh Nalpet Adimurthy <adesh_nalpet@Adeshs-MacBook-Pro.local>
2021-11-23 01:06:15 -0400

    Add application main class for user input

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,4 +1,13 @@
 package com.example;
 
+
+import java.util.Scanner;
+
 public class Application {
+
+    public static void main(String[] args) {
+
+        final Scanner scanner = new Scanner(System.in);
+
+    }
 }
