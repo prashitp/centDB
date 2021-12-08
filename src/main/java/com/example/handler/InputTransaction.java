22bac6e3293c4522a36ee7b70ebf1266ede1eed4
Adesh Nalpet Adimurthy <adesh_nalpet@Adeshs-MacBook-Pro.local>
2021-12-08 03:20:52 -0400

    Initial commit for transactions

--- src/main/java/com/example/handler/InputTransaction.java
+++ src/main/java/com/example/handler/InputTransaction.java
@@ -1,9 +1,8 @@
 package com.example.handler;
 
-import com.example.models.*;
-import com.example.models.context.LogContext;
+import com.example.models.Metadata;
+import com.example.models.TableQuery;
 import com.example.models.enums.Operation;
-import com.example.models.erd.Erd;
 import com.example.services.LogService;
 import com.example.services.accessor.FileAccessorImpl;
 import com.example.services.metadata.MetadataService;
@@ -11,32 +10,30 @@ import com.example.services.metadata.MetadataServiceImpl;
 import com.example.services.parser.DatabaseParser;
 import com.example.services.parser.TableParser;
 import com.example.services.processor.TableProcessor;
-import com.example.util.QueryUtil;
 import lombok.SneakyThrows;
 
 import java.util.*;
 
+public class InputTransaction {
 
-public class InputOperation {
-
-    private static Metadata metadata;
     private static LogService logService;
+    private static List<TableQuery> tableQueries = new ArrayList<>();
 
-    public static void query(Scanner scanner) {
-        QUERY: do {
+    public static void query(Scanner scanner, Metadata metadata) {
+        TRANSACTION: do {
             try {
-                System.out.print("SQL> ");
+                System.out.print("TRANSACTION> ");
                 final String query = scanner.nextLine();
 
                 logService = new LogService();
-                operate(scanner, query.toUpperCase(Locale.ROOT));
+                operate(scanner, metadata, query);
             } catch (Exception e) {
-                continue QUERY;
+                continue TRANSACTION;
             }
         } while (true);
     }
 
-    public static void operate(Scanner scanner, String query) {
+    public static void operate(Scanner scanner, Metadata metadata, String query) {
         List<String> strings = Arrays.asList(query.split("\\s"));
         Operation operation = Operation.valueOf(strings.get(0).trim().toUpperCase(Locale.ROOT));
 
@@ -49,10 +46,6 @@ public class InputOperation {
 
             @Override
             public Void visitUse() {
-                metadata = databaseParser.use(query);
-                checkDatabase(metadata);
-                System.out.printf("%s selected \n", metadata.getDatabaseName());
-                logService.log("Database Selected");
                 return null;
             }
 
@@ -68,38 +61,30 @@ public class InputOperation {
 
             @Override
             public Void visitInsert() {
-                checkDatabase(metadata);
                 TableQuery tableQuery = tableParser.insert(query, metadata);
-                tableProcessor.insert(tableQuery);
+                tableQueries.add(tableQuery);
                 return null;
             }
 
             @Override
             @SneakyThrows
             public Void visitSelect() {
-                checkDatabase(metadata);
-                logService.log("Selection started");
                 TableQuery tableQuery = tableParser.select(query, metadata);
-                List<Row> rows = tableProcessor.select(tableQuery);
-
-                LogContext.setTable(QueryUtil.getTable(metadata, tableQuery.getTableName()));
-                logService.log("Selection completed");
+                tableQueries.add(tableQuery);
                 return null;
             }
 
             @Override
             public Void visitUpdate() {
-                checkDatabase(metadata);
                 TableQuery tableQuery = tableParser.update(query, metadata);
-                tableProcessor.update(tableQuery);
+                tableQueries.add(tableQuery);
                 return null;
             }
 
             @Override
             public Void visitDelete() {
-                checkDatabase(metadata);
                 TableQuery tableQuery = tableParser.delete(query, metadata);
-                tableProcessor.delete(tableQuery);
+                tableQueries.add(tableQuery);
                 return null;
             }
 
@@ -110,25 +95,9 @@ public class InputOperation {
 
             @Override
             public Void visitStartTransaction() {
-                checkDatabase(metadata);
-                InputTransaction.query(scanner, metadata);
                 return null;
             }
         });
-    }
 
-    private static void checkDatabase(Metadata metadata) {
-        if (Objects.isNull(metadata)) {
-            System.out.print("Please select database \n");
-        } else {
-            LogContext.setMetadata(metadata);
-        }
-    }
-
-    public static void generateERD(Scanner scanner) {
-        System.out.println("Enter the database name : ");
-        final String databaseName = scanner.nextLine();
-        Erd erd = new Erd();
-        erd.generate(databaseName);
     }
 }
